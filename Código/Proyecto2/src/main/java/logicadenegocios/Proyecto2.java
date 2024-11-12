3d5709fd48bbdd6c84b4786e2a051f68654a8e76
MaikelFn <159388957+MaikelFn@users.noreply.github.com>
2024-11-12 15:04:15 -0600

    Update Proyecto2.java

--- Código/Proyecto2/src/main/java/logicadenegocios/Proyecto2.java
+++ Código/Proyecto2/src/main/java/logicadenegocios/Proyecto2.java
@@ -11,6 +11,6 @@ package logicadenegocios;
 public class Proyecto2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!.");
     }
 }
